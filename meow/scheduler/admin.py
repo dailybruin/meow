c92b2f7c64233b841cd4effe7832dd51699747bc
Byron Lutz <Byron Lutz>
2013-09-08 23:55:07 +0000

    Added model

---
+++ meow/scheduler/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from scheduler.models import SMPost, Section
+
+admin.site.register(SMPost)
+admin.site.register(Section)
--- meow/scheduler/models.py
+++ meow/scheduler/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class SMPost(models.Model):
+    slug = models.CharField(max_length=100, null=True, blank=False)
+    pub_date = models.DateField(null=True)
+    pub_time = models.TimeField(null=True)
+    story_url = models.URLField(max_length=500, null=True)
+    post_twitter = models.TextField(null=True)
+    post_facebook = models.TextField(null=True)
+    section = models.ForeignKey('Section')
+    pub_ready_copy = models.BooleanField(default=False, help_text="Is this copy-edited?")
+    pub_ready_online = models.BooleanField(default=False, help_text="Is this ready to send out?")
+    sent = models.BooleanField(default=False, help_text="Sent out? This should never be set manually.")
+
+class Section(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    twitter_account_handle = models.CharField(max_length=100, null=True)
+    facebook_account_handle = models.CharField(max_length=100, null=True)
+    post_to_main = models.BooleanField(default=True, help_text="Whenever a post to this section is made, also post it to the main newsroom social media accounts?")    
