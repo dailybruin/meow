c4c2635010a2ec0c792e71ee20bafc18663b8bb8
Byron Lutz <Byron Lutz>
2013-09-09 00:36:31 +0000

    Twitter posting works

--- meow/scheduler/management/commands/sendposts.py
+++ meow/scheduler/management/commands/sendposts.py
@@ -1,46 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from scheduler.models import *
-import oauth2 as oauth
-import time
+import tweepy
 
 class Command(BaseCommand):
     help = "Sends the appropriate social media posts"
 
     def handle(self, *args, **options):
-        # Set the API endpoint 
-        url = "https://api.twitter.com/1.1/statuses/update.json"
-
-        # Set the base oauth_* parameters along with any other parameters required
-        # for the API call.
-        params = {
-            'oauth_version': "1.0",
-            'oauth_nonce': oauth.generate_nonce(),
-            'oauth_timestamp': int(time.time()),
-            'status': 'This is a test tweet',
-            
-        }
-
-        # Set up instances of our Token and Consumer. The Consumer.key and 
-        # Consumer.secret are given to you by the API provider. The Token.key and
-        # Token.secret is given to you after a three-legged authentication.
-        token = oauth.Token(key="1983491-iw1d2zJGS36EJ5Mq9wgPWQh4nCIWqfelmwaOfjQc", secret="8uZfKMoh7VjzkJJtjZO9RnPT8bftv0DKFyzfz31E")
-        consumer = oauth.Consumer(key="OtVavzvwyUfOlETpDOg", secret="0wUHlrQSvB8GMHWDoCdFNjwrmDddug9AjUBjh2qE")
-
-        # Set our token/key parameters
-        params['oauth_token'] = token.key
-        params['oauth_consumer_key'] = consumer.key
-
-        # Create our request. Change method, etc. accordingly.
-        req = oauth.Request(method="POST", url=url, parameters=params)
-
-        # Sign the request.
-        signature_method = oauth.SignatureMethod_HMAC_SHA1()
-        req.sign_request(signature_method, consumer, token)
+        CONSUMER_KEY = "OtVavzvwyUfOlETpDOg"
+        CONSUMER_SECRET = "0wUHlrQSvB8GMHWDoCdFNjwrmDddug9AjUBjh2qE"
+        ACCESS_KEY = "1983491-SphvxRyfObEscR0uxzZrEcIiCVEjyiQIbQiNJjH4"
+        ACCESS_SECRET = "Sb7PBeCTRVx6AdnUHpDoXAHWwmtHIm9V9Oxk8k2s8w"
         
-        client = oauth.Client(consumer, access_token)
         
-        # Send the request
-        resp, content = client.request(request_token_url, "GET")
-        print resp
-        print content
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        
+        api = tweepy.API(auth)
+        api.update_status('test tweet. please ignore.')
         
