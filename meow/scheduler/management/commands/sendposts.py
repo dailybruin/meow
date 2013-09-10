774b03c017ab4813c1728885e6c15fffe66cb45a
Byron Lutz <Byron Lutz>
2013-09-10 06:27:35 +0000

    Meow knows how to post to fb

--- meow/scheduler/management/commands/sendposts.py
+++ meow/scheduler/management/commands/sendposts.py
@@ -8,7 +8,8 @@ import tweepy
 from facepy import GraphAPI
 from scheduler.models import *
 from datetime import datetime
-from urllib2 import urlopen
+import urllib2
+from bs4 import BeautifulSoup
 
 class Command(BaseCommand):
     help = "Sends the appropriate social media posts"
@@ -49,15 +50,42 @@ class Command(BaseCommand):
         graph = GraphAPI('CAAAAENUzY8ABAGsDJqlrs0yNcLbq8tHAxFFoZBpkneXLZBaeAwMDnJUDEiROJZC9H9Pt7trXF4ZC8xRsM8ptfUGRP6RvBi8VlW73x8zzDqaor6VYboHs5XUxFaQ3TK9qjkqyQFMbx7yu801CJdxlx43LOZCk4h2pelKvqgrPaNkDQ0VTeJWuatBwPO6vW7QQZD') # This should not expire
 
 
-        # Post a photo of a parrot
-        graph.post(
-            path = '668459723166194/photos',
-            message = smpost.post_facebook,
-            link = smpost.story_url,
-            picture = "http://dailybruin.com/images/2013/01/dailybruinicon2.jpeg",
-            type= "photo",
-            source = urlopen('http://dailybruin.com/images/2013/09/d0148560-ba9c-4ce7-87fe-deee9935ab5b-640x418.jpg'),
-        )
+        # Get the post HTML
+        usock = urllib2.urlopen(smpost.story_url)
+        post_html = usock.read()
+        usock.close()
+        post_dom = BeautifulSoup(post_html)
+        post_image_tag = post_dom.find('img', "wp-post-image")
+        
+        # Detect if the post has an actual image/src we can use (why not be too careful)
+        has_image = False;
+        if post_image_tag is not None:
+            post_image_url = post_image_tag.attrs['data-lazy-src']
+            if post_image_url is None:
+                post_image_tag.attrs['src']
+            # Set the flag
+            if post_image_url is not None:
+                has_image = True
+        
+        ### Now actually post to Facebook
+        
+        # The story does not have an image
+        if has_image is False:
+            graph.post(
+                path = '668459723166194/feed',
+                message = smpost.post_facebook,
+                link = smpost.story_url,
+                picture = "http://dailybruin.com/images/2013/01/dailybruinicon2.jpeg",
+            )
+        
+        # The story has an image
+        else:
+            graph.post(
+                path = '668459723166194/photos',
+                message = smpost.post_facebook + "\n\nRead more: " + smpost.story_url,
+                type= "photo",
+                source = urllib2.urlopen(post_image_url),
+            )
 
     def handle(self, *args, **options):
         # Get posts from the database
@@ -77,6 +105,7 @@ class Command(BaseCommand):
             if post.post_facebook is not None:
                 self.sendFacebookPost(post)
             if post.post_twitter is not None:
+                break
                 self.sendTweet(post)
             post.sent = True
             post.save() # Save that we sent the post
