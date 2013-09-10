5ce9ec1ab6e8775673423ac5aff90bfa82e9c944
Byron Lutz <Byron Lutz>
2013-09-10 07:34:53 +0000

    Made a new settings class and changed the way the default account works

--- meow/scheduler/admin.py
+++ meow/scheduler/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from scheduler.models import SMPost, Section
+from scheduler.models import SMPost, Section, MeowSetting
 
 admin.site.register(SMPost)
 admin.site.register(Section)
+admin.site.register(MeowSetting)
--- meow/scheduler/models.py
+++ meow/scheduler/models.py
@@ -11,13 +11,26 @@ class SMPost(models.Model):
     pub_ready_copy = models.BooleanField(default=False, help_text="Is this copy-edited?")
     pub_ready_online = models.BooleanField(default=False, help_text="Is this ready to send out?")
     sent = models.BooleanField(default=False, help_text="Sent out? This should never be set manually.")
+    
+    def __unicode__(self):
+        return self.slug
 
 class Section(models.Model):
     name = models.CharField(max_length=100, blank=False)
     twitter_account_handle = models.CharField(max_length=100, null=True, blank=True)
     facebook_account_handle = models.CharField(max_length=100, null=True, blank=True)
-    post_to_main = models.BooleanField(default=True, help_text="Whenever a post to this section is made, also post it to the main newsroom social media accounts?")
+    also_post_to = models.ForeignKey('Section', blank=True)
     twitter_access_key = models.CharField(max_length=500, null=True, blank=True)
     twitter_access_secret = models.CharField(max_length=500, null=True, blank=True)
     facebook_key = models.CharField(max_length=500, null=True, blank=True)
     facebook_page_id = models.CharField(max_length=200, null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.name
+
+class MeowSetting(models.Model):
+    setting_key = models.CharField(max_length=100, blank=False, unique=True)
+    setting_value = models.CharField(max_length=500)
+    
+    def __unicode__(self):
+        return self.setting_key
