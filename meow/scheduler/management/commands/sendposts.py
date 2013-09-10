b125339e6319b039cd844d84e2652ad1a211e69b
Byron Lutz <Byron Lutz>
2013-09-10 04:03:01 +0000

    So this is how you post a picture. Maybe default picture should be part of the section

--- meow/scheduler/management/commands/sendposts.py
+++ meow/scheduler/management/commands/sendposts.py
@@ -11,7 +11,7 @@ from scheduler.models import *
 class Command(BaseCommand):
     help = "Sends the appropriate social media posts"
     
-    def postTweet(self, smpost):
+    def sendTweet(self, smpost):
         print smpost.post_twitter
         CONSUMER_KEY = "OtVavzvwyUfOlETpDOg"
         CONSUMER_SECRET = "0wUHlrQSvB8GMHWDoCdFNjwrmDddug9AjUBjh2qE"
@@ -26,35 +26,29 @@ class Command(BaseCommand):
         api.update_status(smpost.post_twitter)
         
 
+    def sendFacebookPost(self, smpost):
+        print smpost.post_facebook
+        #follow these steps: http://stackoverflow.com/questions/17620266/getting-a-manage-page-access-token-to-upload-events-to-a-facebook-page
+        #Facebook needs the following permissions:
+        # status_update, manage_pages
+        
+        
+        # Initialize the Graph API with a valid access token (optional,
+        # but will allow you to do all sorts of fun stuff).
+        
+        # Get token from here: https://developers.facebook.com/docs/opengraph/howtos/publishing-with-app-token/
+        # appGraph = GraphAPI('72296391616|_vtz8ShgOfzLSgKeDw2quIS1pCc')
+        graph = GraphAPI('CAAAAENUzY8ABAGsDJqlrs0yNcLbq8tHAxFFoZBpkneXLZBaeAwMDnJUDEiROJZC9H9Pt7trXF4ZC8xRsM8ptfUGRP6RvBi8VlW73x8zzDqaor6VYboHs5XUxFaQ3TK9qjkqyQFMbx7yu801CJdxlx43LOZCk4h2pelKvqgrPaNkDQ0VTeJWuatBwPO6vW7QQZD') # This should not expire
+
+
+        # Post a photo of a parrot
+        graph.post(
+            path = '668459723166194/feed',
+            message = smpost.post_facebook,
+            link = smpost.story_url,
+            picture = "http://dailybruin.com/images/2013/01/dailybruinicon2.jpeg",
+        )
+
     def handle(self, *args, **options):
         p = SMPost.objects.get(pk=1)
-        self.postTweet(p)
-#         CONSUMER_KEY = "OtVavzvwyUfOlETpDOg"
-#         CONSUMER_SECRET = "0wUHlrQSvB8GMHWDoCdFNjwrmDddug9AjUBjh2qE"
-#         ACCESS_KEY = "1983491-SphvxRyfObEscR0uxzZrEcIiCVEjyiQIbQiNJjH4"
-#         ACCESS_SECRET = "Sb7PBeCTRVx6AdnUHpDoXAHWwmtHIm9V9Oxk8k2s8w"
-#         
-#         
-#         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-#         auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-#         
-#         api = tweepy.API(auth)
-#         #api.update_status('test tweet. please ignore.')
-#         
-#         #######
-#         #Facebook needs the following permissions:
-#         # status_update, manage_pages
-#         
-#         
-#         # Initialize the Graph API with a valid access token (optional,
-#         # but will allow you to do all sorts of fun stuff).
-#         
-#         # Get token from here: https://developers.facebook.com/docs/opengraph/howtos/publishing-with-app-token/
-# #        graph = GraphAPI('72296391616|_vtz8ShgOfzLSgKeDw2quIS1pCc')
-#         graph = GraphAPI('CAAAAENUzY8ABAFx3ZBkkbpaGmlgM6q3iYUcO3b0R7wxwEUGTnOQVIbgnvQH0u59w1iqkiwtZCnPq3K5frJQLQ5VnAZBkNGiZAVcudSN4g5o0ZBznBAQsBq8BVoc2vF2ezlSCEEkvaoz0WTANyqoqITcLdj2JjPWgQ9jJwLkrTu5gELvZAE93cLYqHNFQQ2xqcZD')
-# 
-#         # Post a photo of a parrot
-#         graph.post(
-#             path = '668459723166194/feed',
-#             message = 'Kenny is a human bean.'
-#         )
+        self.sendFacebookPost(p)
