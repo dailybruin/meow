68523752b3c07e1274042c3d5f7996c43e457b89
Byron Lutz <byronlutz@gmail.com>
2013-09-14 18:59:50 -0700

    Plumbing for edit post page

--- meow/scheduler/urls.py
+++ meow/scheduler/urls.py
@@ -6,5 +6,6 @@ from scheduler import views
 urlpatterns = patterns('',
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html'}),
+    url(r'^edit/(?P<post_id>\d+)/$', views.edit, name="edit"),
     url(r'^$', views.dashboard, name='dashboard'),
 )
--- meow/scheduler/views.py
+++ meow/scheduler/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from scheduler.models import *
 
 @login_required
@@ -12,6 +12,16 @@ def dashboard(request):
         "smposts" : SMPost.objects.all(),
     }
     return render(request, 'scheduler/dashboard.html', context)
+
+@login_required
+def edit(request, post_id):
+    post = get_object_or_404(SMPost, pk=post_id)
+    context = {
+        "user" : request.user,
+        "sections" : Section.objects.all(),
+        "post" : post,
+    }
+    return render(request, 'scheduler/edit.html', context)
 
 def logout(request):
     return logout(request)
