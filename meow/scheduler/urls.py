f5d1ae3e49c2c57089bd43d8cd2966b3f1908892
Byron Lutz <byronlutz@gmail.com>
2013-09-14 17:17:15 -0700

    Template wiring for dashboard

--- meow/scheduler/urls.py
+++ meow/scheduler/urls.py
@@ -4,8 +4,7 @@ from django.contrib.auth.forms import *
 from scheduler import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.dashboard, name='dashboard'),
     (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'scheduler/login.html'}),
     (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scheduler/login.html'}),
-    
+    url(r'^$', views.dashboard, name='dashboard'),
 )
--- meow/scheduler/views.py
+++ meow/scheduler/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 @login_required
 def dashboard(request):
-    return HttpResponse("This is the dashbaord")
+    context = {"hi":"ho",}
+    return render(request, 'scheduler/dashboard.html', context)
 
 def logout(request):
     return logout(request)
