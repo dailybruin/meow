a67173ffd4fa52fd4f171467f19ab2a87f1822af
Byron Lutz <byronlutz@gmail.com>
2013-09-14 17:25:51 -0700

    Basic info passed to dashboard

--- meow/scheduler/views.py
+++ meow/scheduler/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from scheduler.models import *
 
 @login_required
 def dashboard(request):
-    context = {"hi":"ho",}
+    context = {
+        "user" : request.user,
+        "sections" : Section.objects.all()
+    }
     return render(request, 'scheduler/dashboard.html', context)
 
 def logout(request):
