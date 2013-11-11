d2e5ae6a8a51682ad8856bf201ef7d4b2248daf9
Byron Lutz <byronlutz@gmail.com>
2013-11-10 21:14:24 -0800

    Modified sendposts now works

--- meow/scheduler/management/commands/sendposts.py
+++ meow/scheduler/management/commands/sendposts.py
@@ -13,8 +13,7 @@ import HTMLParser
 class Command(BaseCommand):
     help = "Sends the appropriate social media posts"
 
-    def sendTweet(self, smpost, section, bitly_link):
-        print "Send tweet"
+    def sendTweet(self, smpost, section, url):
         try:
             print smpost.post_twitter.encode('ascii','ignore')
             CONSUMER_KEY = MeowSetting.objects.get(setting_key='twitter_consumer_key').setting_value
@@ -27,16 +26,10 @@ class Command(BaseCommand):
         
             api = tweepy.API(auth)
         
-            # Check for bitly link
-            if bitly_link is not None:
-                url = bitly_link
-            else:
-                url = smpost.story_url
-
             # Make the tweet follow DB social media standards
             tweet = smpost.post_twitter
 
-            if smpost.story_url is not None:
+            if url is not None:
                 tweet = tweet + " " + url
         
             api.update_status(tweet)
@@ -45,8 +38,7 @@ class Command(BaseCommand):
             smpost.log_error(e, section)
         
 
-    def sendFacebookPost(self, smpost, section, bitly_link):
-        print "send fb"
+    def sendFacebookPost(self, smpost, section, url, photo_url):
         try:
             print smpost.post_facebook.encode('ascii','ignore')
             #follow these steps: http://stackoverflow.com/questions/17620266/getting-a-manage-page-access-token-to-upload-events-to-a-facebook-page
@@ -64,60 +56,40 @@ class Command(BaseCommand):
 
             PAGE_ID = section.facebook_page_id
 
-            # Get the post HTML
-            try:
-                usock = urllib2.urlopen(smpost.story_url)
-                post_html = usock.read()
-                usock.close()
-            except urllib2.HTTPError as e:
-                if e.code != 500:
-                    raise
-                else:
-                    smpost.log_error(e, section)
-                    post_html = ""
-            
-            post_dom = BeautifulSoup(post_html)
-            post_image_tag = post_dom.find('img', "wp-post-image")
-        
-            # Detect if the post has an actual image/src we can use (why not be too careful)
-            has_image = False;
-            if post_image_tag is not None:
-                post_image_url = post_image_tag.attrs['data-lazy-src']
-                if post_image_url is None:
-                    post_image_tag.attrs['src']
-                # Set the flag
-                if post_image_url is not None:
-                    has_image = True
-        
-            # Check for bitly link
-            if bitly_link is not None:
-                url = bitly_link
-            else:
-                url = smpost.story_url
-
             ### Now actually post to Facebook
         
-            # The story does not have an image
-            if has_image is False:
-                DEFAULT_PHOTO = MeowSetting.objects.get(setting_key='fb_default_photo').setting_value
-                graph.post(
-                    path = PAGE_ID+'/feed',
-                    message = smpost.post_facebook,
-                    link = smpost.story_url,
-                    picture = DEFAULT_PHOTO,
-                )
-        
-            # The story has an image
-            else:
+            if photo_url and url:
                 graph.post(
                     path = PAGE_ID+'/photos',
                     message = smpost.post_facebook + "\n\nRead more: " + url,
                     type= "photo",
-                    source = urllib2.urlopen(post_image_url),
+                    source = urllib2.urlopen(photo_url),
                 )
-
-        except (FacepyError, FacebookError, OAuthError, HTTPError, SignedRequestError, urllib2.URLError, urllib2.HTTPError, HTMLParser.HTMLParseError) as e:
-            smpost.log_error(e, section)
+            elif photo_url:
+                graph.post(
+                    path = PAGE_ID+'/photos',
+                    message = smpost.post_facebook,
+                    type= "photo",
+                    source = urllib2.urlopen(photo_url),
+                )
+            elif url:
+                DEFAULT_PHOTO = MeowSetting.objects.get(setting_key='fb_default_photo').setting_value
+                graph.post(
+                    path = PAGE_ID+'/feed',
+                    message = smpost.post_facebook,
+                    link = url,
+                    picture = DEFAULT_PHOTO,
+                )
+            else:
+                DEFAULT_PHOTO = MeowSetting.objects.get(setting_key='fb_default_photo').setting_value
+                graph.post(
+                    path = PAGE_ID+'/feed',
+                    message = smpost.post_facebook,
+                    picture = DEFAULT_PHOTO,
+                )
+        
+        except (FacepyError, FacebookError, OAuthError, SignedRequestError, urllib2.URLError, urllib2.HTTPError) as e:
+            smpost.log_error(e, section, True)
 
     def handle(self, *args, **options):
         send_posts = MeowSetting.objects.get(setting_key="send_posts").setting_value
@@ -155,30 +127,35 @@ class Command(BaseCommand):
                 # send_url[0] is canonical. send_url[1] is short url.
                 send_url = post.get_send_url()
                 
+                # This will throw an error if the page cannot be reached
+                photo_url = post.get_post_photo_url()
+                
                 # Post to facebook
                 if post.post_facebook:
                     # Section's account
                     if (post.section.facebook_page_id and post.section.facebook_key):
-                        self.sendFacebookPost(post, post.section, link)
+                        self.sendFacebookPost(post, post.section, send_url[1], photo_url)
                     # Also post to account
                     if (post.section.also_post_to and 
                         post.section.also_post_to.facebook_page_id and post.section.also_post_to.facebook_key):
-                        self.sendFacebookPost(post, post.section.also_post_to, link)
+                        self.sendFacebookPost(post, post.section.also_post_to, send_url[1], photo_url)
                 # Post to twitter
                 if post.post_twitter:
                     # Section's account
                     if (post.section.twitter_access_key and post.section.twitter_access_secret):
-                        self.sendTweet(post, post.section, link)
+                        self.sendTweet(post, post.section, send_url[1])
                     # Also post to account
                     if (post.section.also_post_to and 
                         post.section.also_post_to.twitter_access_key and post.section.also_post_to.twitter_access_secret):
-                        self.sendTweet(post, post.section.also_post_to, link)
+                        self.sendTweet(post, post.section.also_post_to, send_url[1])
             # These are the only cases we'll retry (4xx, 5xx)
             except urllib2.HTTPError as e:
                 # We'll only get here for a non-3xx error. Don't send the post out.
-                post.log_error(e, section, True)
+                post.log_error(e, post.section, True)
             except:
-                pass
+                # Something wrong happened. Don't send this post.
+                e = sys.exc_info()[0]
+                post.log_error(e, post.section, True)
             
             # Now save whatever we changed to the post
             try:    
