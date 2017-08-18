9f67614a18141522f9ac7c97205ac7a9757568cd
Hongyi Zhang <hongee.tc@gmail.com>
2017-08-17 22:54:22 -0700

    raw_input -> input

--- meow/scheduler/management/commands/twitter_auth.py
+++ meow/scheduler/management/commands/twitter_auth.py
@@ -17,7 +17,7 @@ class Command(BaseCommand):
         auth.secure = True
         auth_url = auth.get_authorization_url()
         print('Please authorize: ' + auth_url)
-        verifier = raw_input('PIN: ').strip()
+        verifier = input('PIN: ').strip()
         auth.get_access_token(verifier)
         print("ACCESS_KEY = '%s'" % auth.access_token.key)
         print("ACCESS_SECRET = '%s'" % auth.access_token.secret)
