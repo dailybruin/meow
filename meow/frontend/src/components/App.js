90a04fefc8a56ab64a63c41c9381a4ea18cf044c
Dustin Newman <dustinnewman98@gmail.com>
2018-09-16 18:34:08 -0700

    frontend is wired whoo

---
+++ meow/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <p> Working! </p>
+        );
+    }
+}
+
+export default App;
--- meow/meow/urls.py
+++ meow/meow/urls.py
@@ -4,9 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.LoginView.as_view(
-        template_name='scheduler/login.html'), name="login"),
-    url(r'^logout/$', auth_views.LogoutView.as_view(
-        template_name='scheduler/login.html')),
-    url(r'', include('scheduler.urls')),
+    url(r'', include('frontend.urls')),
 ]
