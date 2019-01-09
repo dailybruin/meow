1758c88e8024d0e11d933c2557003f549368515e
Dustin Newman <dustinnewman98@gmail.com>
2019-01-08 22:32:28 -0800

    got rid of sidebar menu

--- meow/frontend/src/components/Sidebar/index.js
+++ meow/frontend/src/components/Sidebar/index.js
@@ -13,38 +13,7 @@ class Sidebar extends React.Component {
         <div className="logo">
           <h1>meow</h1>
         </div>
-        <Menu defaultSelectedKeys={["/"]} selectedKeys={[location.pathname]}>
-          <Menu.Item key="/">
-            <NavLink to="/">
-              <Icon type="coffee" />
-              <span className="nav-text">Desk</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="/newsroom">
-            <NavLink to="/newsroom">
-              <Icon type="global" />
-              <span className="nav-text">Newsroom</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="/calendar">
-            <NavLink to="/calendar">
-              <Icon type="calendar" />
-              <span className="nav-text">Calendar</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="/members">
-            <NavLink to="/members">
-              <Icon type="team" />
-              <span className="nav-text">Members</span>
-            </NavLink>
-          </Menu.Item>
-          <Menu.Item key="/settings">
-            <NavLink to="/settings">
-              <Icon type="setting" />
-              <span className="nav-text">Settings</span>
-            </NavLink>
-          </Menu.Item>
-        </Menu>
+        <h2>Content here</h2>
       </Sider>
     );
   }
