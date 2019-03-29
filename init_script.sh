d2aeb6f82b50cad41a00b9612123c63662546f16
Neil Prajapati <neilp0101@gmail.com>
2019-03-28 19:26:34 -0700

    writes to the correct .env file and prompts slack stuff

--- init_script.sh
+++ init_script.sh
@@ -4,11 +4,28 @@
 docker-compose down
 
 
-REDIS_POSTGRES="REDIS_URL=redis://redis:6379/\nDATABASE_URL=postgres://postgres@db:5432/postgres\n"
+REDIS_POSTGRES="REDIS_URL=redis://redis:6379/\nDATABASE_URL=postgres://postgres@db:5432/postgres"
 
-if ! grep -q $REDIS_POSTGRES meow/.env; then
-       echo -e  $REDIS_POSTGRES > meow/.env	
+if [ ! -f ".env" ]
+then
+	echo -e  $REDIS_POSTGRES > .env
+	echo -e "\nThe following values can be found in the pinned messages of the #meow-dev channel"
+	read -p "SECRET_KEY: " TMP
+	echo -e "SECRET_KEY=$TMP" >> .env
+
+	read -p "Debug (True/False): " TMP
+	echo -e "Debug=$TMP" >> .env
+
+	read -p "SLACK_CLIENT_ID: " TMP
+	echo -e "SLACK_CLIENT_ID=$TMP" >> .env
+
+	read -p "SLACK_CLIENT_SECRET: " TMP
+	echo -e "SLACK_CLIENT_SECRET=$TMP" >> .env
+
+	read -p "SLACK_ENDPOINT: " TMP
+	echo -e "SLACK_ENDPOINT=$TMP" >> .env
 fi
+
 
 # this inits everything but will only continue if the pervious step was successful
 docker-compose build &&
