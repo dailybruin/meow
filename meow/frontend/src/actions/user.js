8f0029b152794bba940cad2e14a67eda2a8a29ea
Galen Wong <wonggalen1999@gmail.com>
2019-06-03 19:42:29 -0700

    Sign out on `/user/me` failure (#260)
    * fix onRow deprecation warning
    
    * Force logout, regardless of server response.
    
    * Failure on `getMe` will trigger logout
    
    * fix axios calls status checking

--- meow/frontend/src/actions/user.js
+++ meow/frontend/src/actions/user.js
@@ -12,97 +12,84 @@ const loginSuccess = ({ username, firstName, isAuthenticated, theme }) => ({
 
 export const login = () => {
   return dispatch => {
-    return getMe().then(
-      ({ data, status }) => {
-        if (status >= 400) {
-          dispatch({
-            type: "USER_LOGIN_FAIL",
-            message: "Could not get current user."
-          });
-        } else {
-          dispatch(
-            loginSuccess({
-              username: data.username,
-              firstName: data.first_name,
-              theme: data.theme,
-              isAuthenticated: true
-            })
-          );
-        }
-      },
-      err => {
+    return getMe()
+      .then(({ data }) => {
+        dispatch(
+          loginSuccess({
+            username: data.username,
+            firstName: data.first_name,
+            theme: data.theme,
+            isAuthenticated: true
+          })
+        );
+      })
+      .catch(err => {
         dispatch({
-          type: "NETWORK_ERROR",
-          message: "Could not connect to server."
+          type: "USER_LOGIN_FAIL",
+          message: "Could not get current user."
         });
-      }
-    );
+        if (err.response.status >= 500)
+          dispatch({
+            type: "NETWORK_ERROR",
+            message: "Could not connect to server."
+          });
+      });
   };
 };
 
 export const logout = () => dispatch => {
-  return apiLogout()
-    .then(() => {
-      dispatch({
-        type: "LOGOUT"
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: "NETWORK_ERROR",
-        message: "Could not connect to server."
-      });
-    });
+  // We logout frontend no matter what heppens
+  dispatch({
+    type: "LOGOUT"
+  });
+  // we don't care what returns from server
+  apiLogout();
 };
 
 export const getUser = username => {
   return dispatch => {
-    return userDetail(username).then(
-      ({ data, status }) => {
-        if (status >= 400) {
+    return userDetail(username)
+      .then(({ data }) => data)
+      .catch(err => {
+        const { status } = err.response;
+        if (status >= 400 && status < 500)
           dispatch({
             type: "PROFILE_FAIL",
             message: `Could not get specified user "${username}"`
           });
-        } else {
-          return data;
-        }
-      },
-      err => {
-        dispatch({
-          type: "NETWORK_ERROR",
-          message: "Could not connect to server."
-        });
-      }
-    );
+        else
+          dispatch({
+            type: "NETWORK_ERROR",
+            message: "Could not connect to server."
+          });
+      });
   };
 };
 
 export const editUser = newData => dispatch => {
-  return putUser(newData).then(
-    ({ data, status }) => {
-      if (status >= 400) {
+  return putUser(newData)
+    .then(({ data }) => {
+      if (newData.selected_theme) {
+        dispatch({
+          type: "THEME_CHANGE",
+          payload: {
+            theme: newData.selected_theme
+          }
+        });
+      }
+      return data;
+    })
+    .catch(err => {
+      const { status } = err.response;
+      if (status >= 400 && status < 500)
         dispatch({
           type: "EDIT_USER_FAIL",
           message: `Could not edit current user"`
         });
-      } else {
-        if (newData.selected_theme) {
-          dispatch({
-            type: "THEME_CHANGE",
-            payload: {
-              theme: newData.selected_theme
-            }
-          });
-        }
-        return data;
-      }
-    },
-    err => {
-      dispatch({
-        type: "NETWORK_ERROR",
-        message: "Could not connect to server."
-      });
-    }
-  );
+      else
+        dispatch({
+          type: "NETWORK_ERROR",
+          message: "Could not connect to server."
+        });
+    });
 };
