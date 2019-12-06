aaaad4052467f6648d780c026f50ea29692e5a55
Neil Prajapati <neilp0101@gmail.com>
2019-12-05 19:16:04 -0800

    Neil/small things (#307)
    * removed notifications prompt
    
    * Update dependencies related to slack oauth

--- meow/frontend/src/index.js
+++ meow/frontend/src/index.js
@@ -10,19 +10,19 @@ import { store, persistor } from "./store";
 import "./index.css";
 import App from "./components/App";
 
-try {
-  Notification.requestPermission().then(permission => {
-    if (permission === "denied") {
-      // eslint-disable-next-line no-alert
-      alert("Notifications have been disabled");
-    }
-  });
-} catch (error) {
-  // Safari doesn't return a promise for requestPermissions and it
-  // throws a TypeError. It takes a callback as the first argument
-  // instead.
-  console.log("This browser does not support Notifications");
-}
+// try {
+//   Notification.requestPermission().then(permission => {
+//     if (permission === "denied") {
+//       // eslint-disable-next-line no-alert
+//       alert("Notifications have been disabled");
+//     }
+//   });
+// } catch (error) {
+//   // Safari doesn't return a promise for requestPermissions and it
+//   // throws a TypeError. It takes a callback as the first argument
+//   // instead.
+//   console.log("This browser does not support Notifications");
+// }
 
 ReactDOM.render(
   <Provider store={store}>
