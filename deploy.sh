3c814c1d3da80dfb68d177e201f40116ea739568
Neil Prajapati <neilp0101@gmail.com>
2020-01-17 12:11:04 -0800

    deploy script:

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ rm meow/frontend/bundles/* -f
 npm run build-production &&
 docker-compose run web meow/manage.py collectstatic &&
 docker-compose build &&
+docker-compose run web meow/manage.py migrate && 
 docker tag meow dailybruin/meow &&
 docker push dailybruin/meow
 
